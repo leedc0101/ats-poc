b2e7e8184e2b78f03e66329628a5ce89c11663b9
Haru <haru@wrtn.io>
2024-05-22 23:48:49 +0900

    add socket

--- src/component/TradingView/datafeed.ts
+++ src/component/TradingView/datafeed.ts
@@ -1,4 +1,4 @@
-import { REST_API_ENDPOINT } from "@/constant";
+import { REST_API_ENDPOINT, WS_API_ENDPOINT } from "@/constant";
 import { configurationData } from "./config";
 
 // const resolutionMap = {
@@ -10,12 +10,39 @@ import { configurationData } from "./config";
 //   "1D": "1d",
 // };
 
-// const intervalMap = {};
-// const lastBar = {};
+const channelToSubscription = new Map();
+
+const commonSymbolInfo = {
+  timezone: "Asia/Seoul",
+  minmov: 1,
+  minmov2: 0,
+  pointvalue: 1,
+  session: "24x7",
+  has_intraday: true,
+  has_no_volume: false,
+  type: "crypto",
+  supported_resolutions: ["1"],
+  exchange: "Test",
+  has_weekly_and_monthly: false,
+  volume_precision: 2,
+  pricescale: 100,
+};
 
 const symbols = [
-  { name: "btc", type: "crypto" },
-  { name: "eth", type: "crypto" },
+  {
+    name: "btc",
+    description: "btc",
+    symbol: "btc",
+    full_name: "btc",
+    ...commonSymbolInfo,
+  },
+  {
+    name: "eth",
+    description: "eth",
+    symbol: "eth",
+    full_name: "eth",
+    ...commonSymbolInfo,
+  },
 ];
 
 const datafeed = {
@@ -34,6 +61,7 @@ const datafeed = {
         symbol.type === symbolType &&
         symbol.name.toLowerCase().indexOf(userInput.toLowerCase()) !== -1,
     );
+    console.log(symbolInfos);
     setTimeout(() => onResultReadyCallback(symbolInfos));
   },
 
@@ -44,23 +72,7 @@ const datafeed = {
   ) => {
     const symbolInfo = symbols.find((el) => symbolName === el.name);
 
-    // setTimeout(() => onSymbolResolvedCallback(symbolInfo));
-    setTimeout(() =>
-      onSymbolResolvedCallback({
-        name: symbolName,
-        ticker: symbolName,
-        timezone: "Asia/Seoul",
-        minmov: 1,
-        minmov2: 0,
-        pointvalue: 1,
-        session: "24x7",
-        has_intraday: true,
-        has_no_volume: false,
-        description: symbolName,
-        type: "crypto",
-        supported_resolutions: ["1"],
-      }),
-    );
+    setTimeout(() => onSymbolResolvedCallback(symbolInfo));
   },
 
   getBars: async (
@@ -74,8 +86,6 @@ const datafeed = {
 
     const from_date = new Date(from * 1000).toISOString();
     const to_date = new Date(to * 1000).toISOString();
-
-    console.log(from_date, to_date);
 
     const res = await fetch(
       `${REST_API_ENDPOINT}/candle/${symbolInfo.name}/${from_date}/${to_date}`,
@@ -107,10 +117,41 @@ const datafeed = {
     onRealtimeCallback: any,
     subscribeUID: any,
     onResetCacheNeededCallback: any,
-  ) => {},
+  ) => {
+    const socket = new WebSocket(
+      `${WS_API_ENDPOINT}/candle/${symbolInfo.name}`,
+    );
+
+    channelToSubscription.set(subscribeUID, socket);
+
+    socket.onopen = () => {
+      console.log("[socket] Connected");
+    };
+
+    socket.onclose = (reason) => {
+      console.log("[socket] Disconnected:", reason);
+    };
+
+    socket.onerror = (error) => {
+      console.log("[socket] Error:", error);
+    };
+
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      const bar = {
+        ...data,
+      };
+      onRealtimeCallback(bar);
+    };
+  },
 
   unsubscribeBars: (subscriberUID: any) => {
-    // clearInterval(intervalMap[subscriberUID]);
+    console.log(subscriberUID);
+    const socket = channelToSubscription.get(subscriberUID);
+    if (socket) {
+      socket.close();
+      channelToSubscription.delete(subscriberUID);
+    }
   },
 };
 
